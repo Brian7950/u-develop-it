3c4c10d46b7d7afdc16fb7e07e0dd4b9d56022a4
Brian7950 <brianh7950@gmail.com>
2022-04-19 17:32:45 -0600

    db connected to node

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -6,7 +7,22 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+//connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'Theromeofmylife!',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
 
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+})
 //Default response for any other request (Not Found)
 //this route is a catch all, important to place last or it will override other routes.
 app.use((req, res) => {
