a66a9bf57ccd3abcf7e3bbe145a77b11d00715f4
Brian7950 <brianh7950@gmail.com>
2022-04-19 17:00:35 -0600

    connected express

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
-const app = express;
+const app = express();
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.json({
-        message:'Hello World'
-    });
-});
+
+//Default response for any other request (Not Found)
+//this route is a catch all, important to place last or it will override other routes.
+app.use((req, res) => {
+    res.status(404).end();
+})
+
 
 //starts express.js on our port(3001)
 app.listen(PORT, () => {
